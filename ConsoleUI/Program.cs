f49ef1b3dc567133b4d78a1f4d5b19541bee0d2d
Tim Stahl <timothystahl@gmail.com>
2020-01-02 15:34:47 -0700

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SayAuthor();
+
+            string firstName = GetUsersName("What is your first name: ");
+            string lastName = GetUsersName("What is your last name: ");
+           
+            WelcomeUser(firstName, lastName);
+
+            Console.ReadLine();
+        }
+
+        private static void SayAuthor()
+        {
+            Console.WriteLine("**************");
+            Console.WriteLine("Written by: Tim Stahl");
+            Console.WriteLine("**************");
+        }
+
+        private static void WelcomeUser(string firstName, string lastName)
+        {
+            Console.WriteLine($"Hello { firstName } { lastName }");
+        }
+
+        private static string GetUsersName(string message)
+        {
+            Console.Write(message);
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+    }
+}
